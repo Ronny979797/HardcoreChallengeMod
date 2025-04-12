d8b646c93e7d1a4ab6167f4a336032bce7de1dcb
Ronny979797 <xxgoldgamerxx97@gmail.com>
2025-04-12 13:12:01 +0200

    Add HardcoreChallengeMod main class

--- src/main/java/net/hardcorechallenge/HardcoreChallengeMod.java
+++ src/main/java/net/hardcorechallenge/HardcoreChallengeMod.java
@@ -1,1 +1,21 @@
+package net.hardcorechallenge;
 
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HardcoreChallengeMod implements ModInitializer {
+    public static final String MOD_ID = "hardcore_challenge";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Hardcore Challenge Mod loaded!");
+        // Init quests, events, day tracking etc.
+    }
+
+    public static Identifier id(String path) {
+        return new Identifier(MOD_ID, path);
+    }
+}
